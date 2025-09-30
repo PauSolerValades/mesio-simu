a97278d3ef81c99265eb7c3c94a5fbad3961c584
PauSolerValades <pau.soler.valades@gmail.com>
2025-10-01 00:03:41 +0200

    Codi de random number generation fet :)

--- random_num_generation/rng.zig
+++ random_num_generation/rng.zig
@@ -7,6 +7,9 @@ const RNGError = error{
     NotAFloat
 };
 
+
+const size = 1000;
+
 pub fn main() !void {
     // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     // defer arena.deinit();
@@ -19,33 +22,51 @@ pub fn main() !void {
     });
     var rand = prng.random();
     
-    const size = 1000;
     std.debug.print("Let's generate {d} uniform random values\n", .{size});
     
     var uniform_sample: [size]f32 = undefined;
-    
+       
     for (0..size) |i| {
         const u = try runif(f32, 0.0, 1.0, &rand);
         uniform_sample[i] = u;
     }
 
-    //std.debug.print("{any}\n", .{uniform_sample});
-    
-
-    //random_values <- lambda*(-log(U))^(1/k)
-
     std.debug.print("Let's generate {d} weibull random values\n", .{size});
     
     var weibull_sample: [size]f32 = undefined;
-    const lambda = 2;
-    const k: f32 = 10;
+    const lambda: f32 = 2;
+    const k: f32 = 10; 
     for (0.., uniform_sample) |i, u| {
         weibull_sample[i] = lambda*(pow(f32, -@log(u), 1.0 / k));
-        std.debug.print("{d} {d}\n", .{u, weibull_sample[i]});
     }
+    try write_sample_to_file("weibull.csv", &weibull_sample);
+    
+    var gamma_sample: [size]f32 = undefined;
+    const gamma: f32 = 0.5;
+    const x0: f32 = 9;
+    for (0.., uniform_sample) |i, u| {
+        gamma_sample[i] =  x0 + gamma * @tan(std.math.pi*(u - 0.5));
+    }
+    try write_sample_to_file("gamma.csv", &gamma_sample);
 
 }
 
+fn write_sample_to_file(name: []const u8, sample: *[size]f32) !void {
+    const cwd = std.fs.cwd();
+    const file = try cwd.createFile(name, .{});
+    defer file.close();
+
+    var buf: [2048]u8 = undefined;
+    var stdout_file = file.writer(&buf);
+    const writer: *std.Io.Writer = &stdout_file.interface;
+    
+    for (sample) |s| {
+        try writer.print("{d} ", .{s}); 
+    } 
+    try writer.print("\n", .{});
+    try writer.flush();
+    return;
+}
 /// un petit comentari sobre la generació de nombres aleatoris en un ordinador
 /// Hi ha diversos algorismes que et permeten generar nombres aleatòriament.
 /// zig distingeix entre la interfície std.Random i l'algorisme que fa servir
