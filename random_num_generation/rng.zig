edbaa86a37e4fffc3d184e1441fdc7175100c77b
PauSolerValades <pau.soler.valades@gmail.com>
2025-10-03 21:19:30 +0200

    he trigat molt mes del que m'agradria admetre a fer aquesta merda però ja funciona

--- random_num_generation/rng.zig
+++ random_num_generation/rng.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const Random = std.Random;
 const pow = std.math.pow;
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 
 const RNGError = error{
     InvalidRange,
@@ -11,10 +13,6 @@ const RNGError = error{
 const size = 1000;
 
 pub fn main() !void {
-    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena.deinit();
-    // const allocator = arena.allocator();
-
     var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
@@ -32,7 +30,14 @@ pub fn main() !void {
     }
 
     std.debug.print("Let's generate {d} weibull random values\n", .{size});
-    
+     
+    var exp_sample: [size]f32 = undefined;
+    const lambda: f32 = 2;
+    for (0.., uniform_sample) |i, u| {
+        exp_sample[i] = lambda*(-@log(u));
+    }
+    try write_sample_to_file("exp.csv", &exp_sample);
+
     var weibull_sample: [size]f32 = undefined;
     const lambda: f32 = 2;
     const k: f32 = 10; 
@@ -67,6 +72,7 @@ fn write_sample_to_file(name: []const u8, sample: *[size]f32) !void {
     try writer.flush();
     return;
 }
+
 /// un petit comentari sobre la generació de nombres aleatoris en un ordinador
 /// Hi ha diversos algorismes que et permeten generar nombres aleatòriament.
 /// zig distingeix entre la interfície std.Random i l'algorisme que fa servir
@@ -84,9 +90,36 @@ fn runif(comptime T: type, a: T, b: T, rng: *Random) !T {
     }
     
     if (b < a) {
-        std.debug.print("a must be lower than b\n", .{});
         return RNGError.InvalidRange;
     }
-    
+    // scale if needed 
     return a + (b - a) * rng.float(T);
 }
+
+///to mimic R behaviour, we implement an allocator to do it dinamically
+fn rwbSampleAlloc(allocator: Allocator, size: n, comptime T: type, lambda: T, k: T, rng: *Random) !ArrayList(T) {
+    var sample: ArrayList(T) = .empty;
+
+    for (0..n) |i| {
+        const u = try runif(T, 0, 1, rng);
+        const w = lambda*(pow(f32, -@log(u), 1.0 / k));
+        _ = try sample.append(w);
+    }
+
+    return sample;
+}
+
+/// the optimal way is not to call runifSampleAlloc and then multiply all the value or the inverse cdf,
+/// because the cost of the fetching the data are far worse than the extra two evaluations.
+fn runifSampleAlloc(allocator: Allocator, size: n, comptime T: type, a: T, b: T, rng: *Random) !ArrayList(T) {
+    var sample: ArrayList(T) = .empty;
+
+    for (0..n) |i| {
+        const u = try runif(T, a, b, rng);
+        _ = try sample.append(allocator, u);
+    }
+
+    return sample;
+}
+
+
