32d7f98e283d357ffb86868ad607129f6918fe70
PauSolerValades <pau.soler.valades@gmail.com>
2025-10-14 12:22:50 +0200

    fet (més o menys) el segon test

--- hipotesis_testing/permutation_tests.py
+++ hipotesis_testing/permutation_tests.py
@@ -68,7 +68,6 @@ def _(mo):
     ### 3.2 Trobem l'estadístic mostral
 
     $F_M$ serà el valor de l'$F$ estadístic de la mostra actual, és a dir, realitzar un ANOVA sobre els tres grups A, B i C.
-
     """
     )
     return
@@ -99,7 +98,6 @@ def _(mo):
     Tenim tres grups de persones completament diferents, això ens dona un total de 24 observacions. Aquestes persones es poden partir en tres grups, A, B, C iguals, i és clar que la permutació que volem realitzar és moure una mostra del grup A a un altre grup i observar com canvia l'ANOVA. Això és clarament 
 
     $$R_{8,8,8}^{24} = \binom{24}{8} \binom{16}{8} \binom{8}{8}$$
-
     """
     )
     return
@@ -123,7 +121,6 @@ def _(mo):
     ### 3.4 Trobar la distribució sota $H_0$
 
     Utilitzant un subconjunt de permutacions informatives, generem la distribució sota la hipòtesi nula.
-
     """
     )
     return
@@ -134,11 +131,11 @@ def _(A, B, C, np, stats):
     n_resamples = 9999 
     total_sample = np.concatenate((A, B, C))
 
-    permutations = [np.random.permutation(total_sample) for _ in range(0,n_resamples)]
+    perms = [np.random.permutation(total_sample) for _ in range(0,n_resamples)]
 
     f_statistics = [
         stats.f_oneway(element[0:8], element[8:16], element[16:24]).statistic
-        for element in permutations 
+        for element in perms
     ]
     return f_statistics, n_resamples
 
@@ -202,17 +199,122 @@ def _(mo):
         r"""
     # Cas d'estudi 2: Tast de vins 2. 
 
-    Imagina ara que en comptes de 24 mostres, les mateixes 8 persones fan el test A, B, i C. Un cop això és així, podem seguir demostrant la mateixa hipòtesi?
+    Assumim ara que en comptes de 24 mostres, les mateixes 8 persones fan el test A, B, i C. Un cop això és així, podem seguir demostrant la mateixa hipòtesi?
 
+    ## 1. Test
 
+    Seguirem utilitzant un ANOVA entre els tres grups.
+
+    ## 2. Hipòtesi Nul·la
+
+    Utilitzem la mateixa hipòtesi nul·la
+
+    $$H_0: \mu_A = \mu_B = \mu_C$$
+
+    ## 3. Càlcul del $p$-valor
+
+    Al utilitzar el mateix estadístic, ja tenim el còmput mostral. El que canvia ara són les permutacions.
+
+    Al primer cas d'estudi, permutavem l'etiqueta A, B o C segons el grup. Ara però, no té sentit permutar etiquetes de persones diferents, així que per a cada individu només tenim $3!$ posicions. Sabent el nombre de mostres obtenim
+
+    $$R_3_^8 = (3!)^8$$
     """
     )
     return
 
 
 @app.cell
+def _():
+    from math import factorial
+
+    num_perm_total = factorial(3)**8
+    print(f"Hi ha un total de {num_perm_total} ({num_perm_total:.2e}) combinacions")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""El nombre és adequat per un test de MonteCarlo, però és prou petit com per fer un test exacte si ho preferissim.""")
+    return
+
+
+@app.cell
+def _(df1, np):
+    from itertools import permutations, product
+
+    combinations_per_individual = np.array([[*permutations(row)] for row in df1.iter_rows()])
+
+    # funció que implementa el producte cartesià recursiu
+    def get_all_combinations_recursive(list_of_lists):
+        # Base Case: If there's only one list left, return its items.
+        # Each item needs to be wrapped in a list so it can be combined later.
+        if len(list_of_lists) == 1:
+            return [[item] for item in list_of_lists[0]]
+
+        # Recursive Step
+        # 1. Get all combinations from the REST of the lists.
+        combinations_from_rest = get_all_combinations_recursive(list_of_lists[1:])
+    
+        # 2. Get the choices from the CURRENT list.
+        current_choices = list_of_lists[0]
+    
+        all_combinations = []
+        for choice in current_choices:
+            for combo in combinations_from_rest:
+                all_combinations.append([choice] + combo)
+            
+        return all_combinations
+
+    #all_combinations = get_all_combinations_recursive(combinations_per_individual)
+    # fent servir l'unpakcing operator tot va finíssim
+    all_combinations_iterator = np.array(list(product(*combinations_per_individual)))
+    print(len(all_combinations_iterator))
+    return (all_combinations_iterator,)
+
+
+@app.cell
+def _(all_combinations_iterator, np, stats):
+    all_combinations_colums = np.array([array.reshape(array.shape[1], array.shape[0]) for array in all_combinations_iterator])
+
+    f_statistics_exact = [
+        stats.f_oneway(A, B, C).statistic
+        for A, B, C in all_combinations_colums
+    ]
+
+    print(len(f_statistics_exact))
+    return (all_combinations_colums,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""Per a fer-ho amb Monte-Carlo, al ja haver calculat totes les permutacions possibles, només hem d'agafar una mostra de `n_sample` aleatòriament.""")
+    return
+
+
+@app.cell
+def _(all_combinations_colums, n_resamples, np, stats):
+    sample_idx = np.random.choice([i for i in range(len(all_combinations_colums))], size=n_resamples)
+
+    f_statistics_mc = [
+        stats.f_oneway(A, B, C).statistic
+        for (A, B, C) in all_combinations_colums[sample_idx] 
+    ]
+
+    print(len(f_statistics_mc))
+    return (f_statistics_mc,)
+
+
+@app.cell
+def _(f_sample, f_statistics_mc, n_resamples):
+    extreme_observations_2 = (sum((f_sample <= f_statistic for f_statistic in f_statistics_mc)) + 1)
+    pvalue_2 = extreme_observations_2 / (n_resamples +1)
+    print(f"El p-valor és de {pvalue_2:.3e}. Hi ha {extreme_observations_2} observacions sobre F mostral")
+    return
+
+
+@app.cell
 def _(pl):
-    df3 = pl.read_csv("casestudy3.csv")
+    df3= pl.read_csv("casestudy3.csv")
 
     df3
     return
